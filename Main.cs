c13c5592b167dff4b804dde84ac2fe9201df80aa
propstg <g@prop.st>
2024-01-22 00:02:16 -0500

    initial commit

---
+++ Main.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace StargateResolutionFix {
+
+    [BepInProcess("stargate.exe")]
+    [BepInPlugin("blargle.stargate.menufix", "menu fix", "0.1.0")]
+    public class Main : BaseUnityPlugin {
+
+        public void Awake() {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), "blargle.stargate.menufix");
+        }
+    }
+
+    [HarmonyPatch(typeof(MainMenuManager), "MainPanel")]
+    class MainMenuManager_Patch {
+        static bool alreadyAdjusted = false;
+        static Quaternion rotation;
+
+        public static void Postfix(SettingsPanel ___settingsPanel, GameObject ___mainPanelCamera) {
+            if (!alreadyAdjusted) {
+                rotation = ___mainPanelCamera.transform.rotation;
+                alreadyAdjusted = true;
+            }
+
+            Quaternion newRotation = Quaternion.Euler(rotation.eulerAngles.x, rotation.eulerAngles.y - 10f, rotation.eulerAngles.z);
+            ___mainPanelCamera.transform.rotation = newRotation;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# stargate: timekeepers menu fix
+
+Workaround for the Stargate: Timekeepers demo displaying only a 3rd of the screen, with the settings menu button being off screen.
+
+Slightly rotates the camera on the main menu, allowing you to access the Settings menu to fix the problem.
+
+1. Install Bepinex + the mod
+2. Launch the game
+3. Create a profile, if needed
+4. Change your resolution in the settings menu
+5. Uninstall this mod
+
+## Requirements
+
+* BepinEx >= 5.4.21
+
+## Installation
+
+* Install BepinEx in the game folder normally
+* Download StargateMenuFix.dll and add to your `PlateUp/BenInEx/plugins` folder.
